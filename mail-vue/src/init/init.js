abd170eac41f7defb5f5d66626a1df285e3585fc
Right202209 <3118138007@qq.com>
2026-03-09 21:27:22 +0800

    fix: 修复启动卡 loading 并调整 Cloudflare 构建
    确保前端初始化失败时也会移除首屏 loading 遮罩，同时统一 Cloudflare 相关构建配置与 pnpm 版本约束以提升部署稳定性。

--- mail-vue/src/init/init.js
+++ mail-vue/src/init/init.js
@@ -27,37 +27,44 @@ export async function init() {
 
     let setting = null;
 
-    if (token) {
-        const userPromise = loginUserInfo().catch(e => {
-            console.error(e);
-            return null;
-        });
-
-        const [s, user] = await Promise.all([websiteConfig(), userPromise]);
-        setting = s;
-        settingStore.settings = setting;
-        settingStore.domainList = setting.domainList;
-        document.title = setting.title;
-
-        if (user) {
-            accountStore.currentAccountId = user.account.accountId;
-            accountStore.currentAccount = user.account;
-            userStore.user = user;
-
-            const routers = permsToRouter(user.permKeys);
-            routers.forEach(routerData => {
-                router.addRoute('layout', routerData);
+    try {
+        if (token) {
+            const userPromise = loginUserInfo().catch(e => {
+                console.error(e);
+                return null;
             });
+
+            const [s, user] = await Promise.all([websiteConfig(), userPromise]);
+            setting = s;
+            settingStore.settings = setting;
+            settingStore.domainList = setting.domainList;
+            document.title = setting.title;
+
+            if (user) {
+                accountStore.currentAccountId = user.account.accountId;
+                accountStore.currentAccount = user.account;
+                userStore.user = user;
+
+                const routers = permsToRouter(user.permKeys);
+                routers.forEach(routerData => {
+                    router.addRoute('layout', routerData);
+                });
+            }
+
+        } else {
+            setting = await websiteConfig();
+            settingStore.settings = setting;
+            settingStore.domainList = setting.domainList;
+            document.title = setting.title;
         }
-
-    } else {
-        setting = await websiteConfig();
-        settingStore.settings = setting;
-        settingStore.domainList = setting.domainList;
-        document.title = setting.title;
+    } catch (e) {
+        console.error(e)
+        if (!settingStore.settings?.title) {
+            document.title = 'Cloud Mail'
+        }
+    } finally {
+        removeLoading();
     }
-
-    removeLoading();
 }
 
 function removeLoading() {
